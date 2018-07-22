fbbc0a713b8adcd24851367829bbebf39097e118
Jürg Lehni <juerg@scratchdisk.com>
2018-07-22 20:55:39 +0200

    Add support for fullscreen mode
    And only apply styling overrides if "window.titleBarStyle" setting is set to "custom"
    
    Closes #8

--- extension.js
+++ extension.js
@@ -7,22 +7,26 @@ const fsOptions = { encoding: 'utf8' }
 
 const patches = {
   'vs/code/electron-main/main.js': [
+    // Change the Electron titleBarStyle to "hidden-inset"
     [
       '.titleBarStyle="hidden",',
       '.titleBarStyle="hidden-inset",'
     ]
   ],
   'vs/workbench/workbench.main.js': [
+    // Never show the TITLEBAR_PART when "window.titleBarStyle" === "custom" 
     [
-      '.isFullscreen=function(){return this._fullscreen',
-      '.isFullscreen=function(){return true'
+      'TITLEBAR_PART:return"custom"===this.getCustomTitleBarStyle()&&!h.isFullscreen()',
+      'TITLEBAR_PART:return false'
     ],
+    // Read and set the activitybar width from the CSS variable
     [
       '"activitybarWidth",{get:function(){return',
-      `"activitybarWidth",{get:function(){this.partLayoutInfo.activitybar.width=parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--traffic-light-width'), 10);return`
+      `"activitybarWidth",{get:function(){if("custom"===this.themeService.configurationService.getValue().window.titleBarStyle)this.partLayoutInfo.activitybar.width=parseInt(window.getComputedStyle(document.documentElement).getPropertyValue("--traffic-light-width"), 10);return`
     ]
   ],
   'vs/workbench/workbench.main.css': [
+    // Add our CSS modifications to the end of the main file
     [
       /$/g, // Append to the end of the file
       readFile('workbench.main.css')
